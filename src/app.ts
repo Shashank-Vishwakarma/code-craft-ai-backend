cdcf6b62bfd57296bdd6e45e8f4da5d960353313
Shashank <shashank78680@gmail.com>
2025-07-07 00:15:51 +0530

    prisma setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express, { Express } from "express"
 
 const app: Express = express();
 
+app.get("/", (req, res)=>{
+    res.json({message: "Hello from code-craft-ai-backend"})
+})
+
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
